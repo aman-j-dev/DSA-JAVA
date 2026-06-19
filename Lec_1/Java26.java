762f0256fb5ce37dc2b0abce28ae80744052253d
Aman <aman.jangra.11.25@gmail.com>
2026-06-19 22:50:31 +0530

    do Additional INFO

---
+++ Lec_1/Java26.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+void main(){
+    System.out.println("Hello");
+}
+
+
+// it is a direct conflict between Java 26's compiler rules and VS Code's project organizer.
+
+
+// in java 26 packages are not allowed , run it like this
+
+// PS D:\Programming\REPO\DSA\JAVA\Lec_1> javac Java26.java
+// PS D:\Programming\REPO\DSA\JAVA\Lec_1> java Java26
+// Hello
