e4ec2f01f15ff2f579919945ca8cedb0d1dd3d28
Aman <aman.jangra.11.25@gmail.com>
2026-07-01 22:06:33 +0530

    do LEC_11 PATTERN

---
+++ Lec_11/pattern4.java
@@ -0,0 +1,16 @@
+package REPO.DSA.JAVA.Lec_11;
+import java.util.Scanner;
+public class pattern4 {
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i =1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n+1 - i ; j++){
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Lec_11/pattern5.java
@@ -0,0 +1,19 @@
+package REPO.DSA.JAVA.Lec_11;
+import java.util.Scanner;
+public class pattern5 {
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int k =1 ; k <= n-i ; k++){
+                System.out.print("  ");
+            }
+            for(int j = 1 ; j <= ((i*2)-1) ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
